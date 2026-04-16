b0db0b055b740abe01742f765ac760bfc231833a
shadrack mason <shadrack.mason@student.moringaschool.com>
2026-04-16 12:23:06 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here!
+const postList = document.getElementById("post-list");
+const starterPosts = [
+  {
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body:
+      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+];
+
+function displayPosts(posts) {
+  postList.innerHTML = "";
+
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+  });
+}
+
+async function fetchPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+  } catch (error) {
+    displayPosts(starterPosts);
+  }
+}
+
+displayPosts(starterPosts);
+fetchPosts();
